f6fa193dc830820b0aa6ef5d199f78981d49b31b
Bukunmi Adeyemo <holuwabukunmie20@gmail.com>
2022-09-09 05:41:52 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,14 +18,17 @@ int main(void)
 	while (n1 <= 57)
 	{
 		n2 = n1 + 1;
+
 		while (n2 <= 57)
 		{
 			n3 = n2 + 1;
+
 			while (n3 <= 57)
 			{	
 				putchar(n1);
 				putchar(n2);
 				putchar(n3);
+
 				if (n1 != 55 || n2 != 56 || n3 != 57)
 				{	
 					putchar(com);
