aca82a1c37d4a6470753cf70df978a14241fd2e5
Bukunmi Adeyemo <holuwabukunmie20@gmail.com>
2022-09-13 07:53:29 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - function that prints 10 times the alphabet in lowercase.
- * Return: always 0.
-*/
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ * Return: Always 0 (Success)
+ */
 void print_alphabet_x10(void)
 {
-	int l;
-	int n = 0;
+	int a, b;
 
-	while (n < 10)
+	for (a = 0; a <= 9; a++)
 	{
-		l = 'a';
-		while (l <= 'z')
+		for (b = 97; b <= 122; b++)
 		{
-			_putchar(l);
-			l += 1;
+			_putchar(b);
 		}
-		_putchar(10);
-		n += 1;
+		_putchar('\n');
 	}
 }
